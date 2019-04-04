9f8bcec3663881b7a3166dde9169f2283c8ca349
David McEldowney <tsax421@gmail.com>
2019-04-04 01:02:01 -0500

    finished the logout template

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# redirect to home URL after login (default redirects to '/accounts/profile/')
+LOGIN_REDIRECT_URL = '/'
+
+
+
